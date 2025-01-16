f399677f954f4dc7cb7ce8063f1c672174d5f083
Lauren J Beesley VanDervort, PhD <31865523+lbeesleyBIOSTAT@users.noreply.github.com>
2025-01-16 13:23:08 -0700

    Add files via upload

---
+++ epifforma/fit_model/run_calibration.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -l
+
+#SBATCH -J calibrate
+#SBATCH -p general
+#SBATCH -c 16
+#SBATCH -t 10:00:00
+#SBATCH --array=1-7
+#SBATCH --output=SLURMOUT/featQuantiles_%A-%a.out
+
+# Load software and activate environment (I loaded the modules into this env already):
+module add R/4.1.2
+R CMD BATCH "--no-save" calibrate_lightgbm.R ./logfiles/grid_logs_$SLURM_ARRAY_TASK_ID.Rout
